9844c08f859b430851f84bcfaf4982c0cddeb41c
nanashi-pootaro <denkgeipoqikpk@gmail.com>
2020-09-16 01:08:43 +0900

    add sidebar

--- main.go
+++ main.go
@@ -1,42 +1,47 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
-        router.LoadHTMLGlob("views/*.html")
-	    router.Static("/assets", "./assets")
+	router := gin.Default()
+	router.LoadHTMLGlob("views/*.html")
+	router.Static("/assets", "./assets")
 
-    router.GET("/dashboard", func(ctx *gin.Context){
-        ctx.HTML(http.StatusOK, "dashboard.html", gin.H{})
-    })
-    
-    router.GET("/icons", func(ctx *gin.Context){
-        ctx.HTML(http.StatusOK, "icons.html", gin.H{})
-    })
+	router.GET("/dashboard", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "dashboard.html", gin.H{})
+	})
 
-    router.GET("/map", func(ctx *gin.Context){
-        ctx.HTML(http.StatusOK, "map.html", gin.H{})
-    })
+	router.GET("/icons", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "icons.html", gin.H{})
+	})
 
-    router.GET("/notifications", func(ctx *gin.Context){
-        ctx.HTML(http.StatusOK, "notifications.html", gin.H{})
-    })
+	router.GET("/map", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "map.html", gin.H{})
+	})
 
-    router.GET("/tables", func(ctx *gin.Context){
-        ctx.HTML(http.StatusOK, "tables.html", gin.H{})
-    })
+	router.GET("/notifications", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "notifications.html", gin.H{})
+	})
 
-    router.GET("/upgrade", func(ctx *gin.Context){
-        ctx.HTML(http.StatusOK, "upgrade.html", gin.H{})
-    })
+	router.GET("/tables", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "tables.html", gin.H{})
+	})
 
-    router.GET("/user", func(ctx *gin.Context){
-        ctx.HTML(http.StatusOK, "user.html", gin.H{})
-    })
+	router.GET("/upgrade", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "upgrade.html", gin.H{})
+	})
 
-    router.Run(":8080")
+	router.GET("/user", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "user.html", gin.H{})
+	})
+
+	router.GET("/sidebar", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "sidebar.html", gin.H{})
+	})
+
+	router.Run(":8080")
 }
